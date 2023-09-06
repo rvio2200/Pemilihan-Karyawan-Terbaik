629f3ebec2b34ff9be447f17f884641825b59a46
rvio2200 <ririnviolina2200@gmail.com>
2023-09-06 12:54:13 +0700

    init

--- arco/metode.php
+++ arco/metode.php
@@ -12,28 +12,29 @@ if (!empty($filter_periode)) {
 $query .= " ORDER BY id_alternatif";
 
 ?>
+
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>PEMILIHAN KARYAWAN TERBAIK</title>
-        
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="icon" type="image/png" href="../assets/desain-login/images/icons/favicon.ico"/>
-        <link rel="stylesheet" type="text/css" href="../assets/desain-login/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-        <link rel="stylesheet" href="../assets/desain-home/css/bootstrap.min.css">
-        <link rel="stylesheet" href="../assets/desain-home/css/bootstrap-theme.min.css">
-        <link rel="stylesheet" href="../assets/desain-home/css/fontAwesome.css">
-        <link rel="stylesheet" href="../assets/desain-home/css/hero-slider.css">
-        <link rel="stylesheet" href="../assets/desain-home/css/owl-carousel.css">
-        <link rel="stylesheet" href="../assets/desain-home/css/datepicker.css">
-        <link rel="stylesheet" href="../assets/desain-home/css/templatemo-style.css">
-        <script src="../assets/desain-home/js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
-        <style>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>PEMILIHAN KARYAWAN TERBAIK</title>
+
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/png" href="../assets/desain-login/images/icons/favicon.ico"/>
+    <link rel="stylesheet" type="text/css" href="../assets/desain-login/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../assets/desain-home/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../assets/desain-home/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="../assets/desain-home/css/fontAwesome.css">
+    <link rel="stylesheet" href="../assets/desain-home/css/hero-slider.css">
+    <link rel="stylesheet" href="../assets/desain-home/css/owl-carousel.css">
+    <link rel="stylesheet" href="../assets/desain-home/css/datepicker.css">
+    <link rel="stylesheet" href="../assets/desain-home/css/templatemo-style.css">
+    <script src="../assets/desain-home/js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+    <style>
         .logo img {
-        max-width: 150px;
+            max-width: 150px;
         }
         /* CSS untuk logo */
         .logo {
@@ -52,74 +53,73 @@ $query .= " ORDER BY id_alternatif";
         .logo-text h3 {
             color: #3B71CA; /* Ganti kode warna dengan kode warna yang Anda inginkan */
         }
-
-
-        </style>    
-    </head>
-
+    </style>
+</head>
 <body>
- 
-    <div class="wrap">
-        <header id="header">
-        <div class="panel panel-container"style="padding: 30px; box-shadow: 2px 2px 5px #BC8F8F; background-color: 	#D8BFD8;">
-                <div class="row">
-                    <div class="col-md-12">
-                        <button id="primary-nav-button" type="button">Menu</button>
-                        <a href="index.php"><div class="logo">
+
+<div class="wrap">
+    <header id="header">
+        <div class="panel panel-container" style="padding: 30px; box-shadow: 2px 2px 5px #BC8F8F; background-color: #D8BFD8;">
+            <div class="row">
+                <div class="col-md-12">
+                    <button id="primary-nav-button" type="button">Menu</button>
+                    <a href="index.php">
+                        <div class="logo">
                             <img src="../assets/desain-home/img/nestle.png" alt="Venue Logo">
                             <div class="logo-text">
-                            <h3><b>PEKANBARU AREA</b></h3>
+                                <h3><b>PEKANBARU AREA</b></h3>
+                            </div>
                         </div>
-                        </div></a>
-                        <nav id="primary-nav" class="dropdown cf">
+                    </a>
+                    <nav id="primary-nav" class="dropdown cf">
                         <ul class="dropdown menu">
-                                <li class="active"><a href="index.php"><span class="fa fa-home"></span><b>&emsp;Home</b></a></li>
-                                <li><a href="alternatif.php"><span class="fa fa-user"></span><b>&emsp;Alternatif</b></a></li>
-                                <li><a class="scrollTo" data-scrollTo="blog" href="kriteria.php"><span class="fa fa-list"></span><b>&emsp;Kriteria</b></a></li>
-                                <li><a class="scrollTo" data-scrollTo="blog" href="penilaian.php"><span class="fa fa-pencil"></span><b>&emsp;Penilaian</b></a></li>
-                                <li><a class="scrollTo" data-scrollTo="services" href="metode.php"><span class="fa fa-refresh"></span><b>&emsp;Metode WP</b></a></li>
-                                <li><a class="scrollTo" data-scrollTo="contact" href="logout.php"><span class="fa fa-power-off"></span><b>&emsp;Logout</b></a></li>
-                            </ul>
-                        </nav>
-                    </div>
+                            <li class="active"><a href="index.php"><span class="fa fa-home"></span><b>&emsp;Home</b></a></li>
+                            <li><a href="alternatif.php"><span class="fa fa-user"></span><b>&emsp;Alternatif</b></a></li>
+                            <li><a class="scrollTo" data-scrollTo="blog" href="kriteria.php"><span class="fa fa-list"></span><b>&emsp;Kriteria</b></a></li>
+                            <li><a class="scrollTo" data-scrollTo="blog" href="penilaian.php"><span class="fa fa-pencil"></span><b>&emsp;Penilaian</b></a></li>
+                            <li><a class="scrollTo" data-scrollTo="services" href="metode.php"><span class="fa fa-refresh"></span><b>&emsp;Metode WP</b></a></li>
+                            <li><a class="scrollTo" data-scrollTo="contact" href="logout.php"><span class="fa fa-power-off"></span><b>&emsp;Logout</b></a></li>
+                        </ul>
+                    </nav>
                 </div>
             </div>
-        </header>
-    </div>
+        </div>
+    </header>
+</div>
 
-
-    <div class="panel panel-container" style="width: 50%; margin: 0 auto; padding: 20px; box-shadow: 2px 2px 5px #888888;">
+<div class="panel panel-container" style="width: 50%; margin: 0 auto; padding: 20px; box-shadow: 2px 2px 5px #888888;">
     <h2 class="text-center"><b>HASIL ANALISA PERHITUNGAN METODE WEIGHTED PRODUCT</b></h2>
 
     <br>
     <br>
     <form action="metode-proses.php" method="POST" class="form-inline" enctype="multipart/form-data">
-    <div class="form-group">
-        <label for="periode">Filter Periode:</label>
-        <select class="form-control" id="periode" name="periode">
-            <option value="">-- Pilih Periode --</option>
-            <?php
-            // Koneksi ke database menggunakan $conn (sesuaikan dengan kode Anda)
-            $sql = "SELECT id_periode, nama_periode FROM tbl_periode";
-            $result = mysqli_query($conn, $sql);
+        <div class="form-group">
+            <label for="periode">Filter Periode:</label>
+            <select class="form-control" id="periode" name="periode">
+                <option value="">-- Pilih Periode --</option>
+                <?php
+                // Koneksi ke database menggunakan $conn (sesuaikan dengan kode Anda)
+                $sql = "SELECT id_periode, nama_periode FROM tbl_periode";
+                $result = mysqli_query($conn, $sql);
 
-            if (mysqli_num_rows($result) > 0) {
-                while ($row = mysqli_fetch_assoc($result)) {
-                    $id_periode = $row['id_periode'];
-                    $nama_periode = $row['nama_periode'];
-                    echo "<option value='$id_periode'>$nama_periode</option>";
+                if (mysqli_num_rows($result) > 0) {
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        $id_periode = $row['id_periode'];
+                        $nama_periode = $row['nama_periode'];
+                        echo "<option value='$id_periode'>$nama_periode</option>";
+                    }
+                } else {
+                    echo "<option value=''>Tidak ada data periode</option>";
                 }
-            } else {
-                echo "<option value=''>Tidak ada data periode</option>";
-            }
 
-            // Tutup koneksi ke database jika sudah selesai
-            mysqli_close($conn);
-            ?>
-        </select>
-    </div>
-    <a href="metode-aksi.php"><button type="submit" class="btn btn-success btn-sm">Filter</button></a>
-</form>
+                // Tutup koneksi ke database jika sudah selesai
+                mysqli_close($conn);
+                ?>
+            </select>
+        </div>
+        <a href="metode-aksi.php"><button type="submit" class="btn btn-success btn-sm">Filter</button></a>
+    </form>
+
     <!-- <style>
     .sticky-title {
         position: sticky;
@@ -173,60 +173,7 @@ $query .= " ORDER BY id_alternatif";
     </div>
     <br>
 
-
-<?php
-//Set Vektor S dan Vektor V
-
-$query=mysqli_query($conn, "SELECT * FROM tbl_alternatif");
-$jumlah=0;
-while ($result=mysqli_fetch_array($query)) {
-    $nomor=$no++;
-    $vektor_s=1;
-    $id=$result['id_alternatif'];
-    $nama=$result['nama_alternatif'];
-
-    //Panggil nilai matriks keputusan
-    $query2=mysqli_query($conn, "SELECT s.nilai_subkriteria as sub,n.id_kriteria as id_kriteria FROM tbl_subkriteria s, tbl_nilai n, tbl_kriteria k WHERE n.id_alternatif='$id' AND s.id_subkriteria=n.id_subkriteria AND k.id_kriteria=n.id_kriteria ORDER BY n.id_kriteria");
-    while ($result2=mysqli_fetch_array($query2)) {
-        $val = $result2['sub'];
-
-        //Panggil nilai bobot
-        $query3=mysqli_query($conn, "SELECT bobot_kriteria FROM tbl_kriteria WHERE id_kriteria='$result2[id_kriteria]'");
-        $result3=mysqli_fetch_assoc($query3);
-        //Normalisasikan nilai bobot kriteria
-        $bobot_k=$result3['bobot_kriteria']/13;
-
-        //Vektor S
-        $val_s = $val ** $bobot_k;
-        $vektor_s *= $val_s;
-
-    }
-
-        //ambil nilai vektor_s simpan ke dalam database
-        mysqli_query($conn, "UPDATE tbl_hasil SET vektor_s ='$vektor_s' WHERE id_alternatif='$id'");
-
-        //Vektor V
-        $query4 = mysqli_query($conn, "SELECT sum(vektor_s) as sum_s FROM tbl_hasil");
-        $b = mysqli_fetch_array($query4);
-        $vektor_v = $vektor_s/$b['sum_s'];
-
-        //ambil nilai vektor_v simpan ke dalam database
-        mysqli_query($conn, "UPDATE tbl_hasil SET vektor_v ='$vektor_v' WHERE id_alternatif='$id'");
-        $jumlah++;
-}
-
-        // Set Ranking
-        $query5 = mysqli_query($conn, "SELECT * FROM tbl_hasil ORDER BY vektor_v DESC");
-        $rank = 1;
-        while ($result5 = mysqli_fetch_array($query5)) {
-            $id_alternatif = $result5['id_alternatif'];
-            mysqli_query($conn, "UPDATE tbl_hasil SET ranking='$rank' WHERE id_alternatif='$id_alternatif'");
-            $rank++;
-}
-
-?>
-
-<style>
+    <style>
     .sticky-title {
         position: sticky;
         top: 0;
@@ -234,10 +181,10 @@ while ($result=mysqli_fetch_array($query)) {
         padding: 10px; /* Atur padding judul sesuai kebutuhan */
     }
     </style>
-<h4 class="modal-title sticky-title"><b>Nilai Vektor S dan Vektor V</b></h4>
-<br>
+    <h4 class="modal-title sticky-title"><b>Nilai Vektor S dan Vektor V</b></h4>
+    <br>
 
-<div class="table-condensed">
+    <div class="table-condensed">
         <style>
         .center-table {
             margin: 0 auto; /* Mengatur margin horizontal menjadi auto */
@@ -254,9 +201,9 @@ while ($result=mysqli_fetch_array($query)) {
             </thead>
             <tbody>
                 <?php
-                    $query = mysqli_query($conn, "SELECT * FROM tbl_hasil order by id_alternatif");
-                    $no=1;
-                    while ($result = mysqli_fetch_array($query)) {
+                $query = mysqli_query($conn, "SELECT * FROM tbl_hasil order by id_alternatif");
+                $no=1;
+                while ($result = mysqli_fetch_array($query)) {
                 ?>
                     <tr>
                         <td class="text-center"><?php echo $no++ ?></td>
@@ -266,7 +213,7 @@ while ($result=mysqli_fetch_array($query)) {
                         
                     </tr>
                 <?php
-                    }
+                }
                 ?>
             </tbody>
         </table>
@@ -281,40 +228,40 @@ while ($result=mysqli_fetch_array($query)) {
         padding: 10px; /* Atur padding judul sesuai kebutuhan */
     }
     </style>
-    
+
     <h4 class="modal-title sticky-title"><b>Hasil Perankingan</b></h4>
-<head>
-<style>
-  .btn {
-    display: inline-block;
-    padding: 10px 20px;
-    border: none;
-    border-radius: 5px;
-    cursor: pointer;
-    text-decoration: none;
-    transition: background-color 0.3s, color 0.3s;
-  }
 
-  .btn-primary {
-    background-color: #3498db;
-    color: white;
-  }
+    <head>
+    <style>
+      .btn {
+        display: inline-block;
+        padding: 10px 20px;
+        border: none;
+        border-radius: 5px;
+        cursor: pointer;
+        text-decoration: none;
+        transition: background-color 0.3s, color 0.3s;
+      }
 
-  .btn-primary:hover {
-    background-color: #2980b9;
-  }
-  .modal-footer .btn {
-  padding: 5px 10px; /* Sesuaikan sesuai kebutuhan Anda */
-  font-size: inherit; /* Mempertahankan ukuran font yang ada */
-}
+      .btn-primary {
+        background-color: #3498db;
+        color: white;
+      }
 
-</style>
-</head>
-<body>
-    <div class="modal-footer">
-        <h4 class="btn btn-primary"><a href="cetak.php" target="_blank" style="color: white;">Cetak</a></h4>
-    </div>
-</body>
+      .btn-primary:hover {
+        background-color: #2980b9;
+      }
+      .modal-footer .btn {
+      padding: 5px 10px; /* Sesuaikan sesuai kebutuhan Anda */
+      font-size: inherit; /* Mempertahankan ukuran font yang ada */
+    }
+    </style>
+    </head>
+    <body>
+        <div class="modal-footer">
+            <h4 class="btn btn-primary"><a href="cetak.php" target="_blank" style="color: white;">Cetak</a></h4>
+        </div>
+    </body>
 
     <div class="table-condensed">
         <style>
@@ -333,9 +280,9 @@ while ($result=mysqli_fetch_array($query)) {
             </thead>
             <tbody>
                 <?php
-                    $query = mysqli_query($conn, "SELECT * FROM tbl_hasil order by ranking");
-                    $no=1;
-                    while ($result = mysqli_fetch_array($query)) {
+                $query = mysqli_query($conn, "SELECT * FROM tbl_hasil order by ranking");
+                $no=1;
+                while ($result = mysqli_fetch_array($query)) {
                 ?>
                     <tr>
                         <td class="text-center"><?php echo $no++ ?></td>
@@ -344,21 +291,19 @@ while ($result=mysqli_fetch_array($query)) {
                         <td class="text-center"><?php echo $result['ranking']; ?></td>
                     </tr>
                 <?php
-                    }
+                }
                 ?>
             </tbody>
         </table>
     </div>
-
 </div>
-</div> -->
+</div>
 
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js" type="text/javascript"></script>
-    <script>window.jQuery || document.write('<script src="../assets/desain-home/js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
-    <script src="../assets/desain-home/js/vendor/bootstrap.min.js"></script>
-    <script src="../assets/desain-home/js/datepicker.js"></script>
-    <script src="../assets/desain-home/js/plugins.js"></script>
-    <script src="../assets/desain-home/js/main.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js" type="text/javascript"></script>
+<script>window.jQuery || document.write('<script src="../assets/desain-home/js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+<script src="../assets/desain-home/js/vendor/bootstrap.min.js"></script>
+<script src="../assets/desain-home/js/datepicker.js"></script>
+<script src="../assets/desain-home/js/plugins.js"></script>
+<script src="../assets/desain-home/js/main.js"></script>
 </body>
 </html>
